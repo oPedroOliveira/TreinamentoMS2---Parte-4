413f9a76fccd7a24ce8b0030e27b57c8c28f5bd8
oPedroOliveira <phoavl07@gmail.com>
2021-03-11 19:46:02 -0300

    Props

--- React/ola-mundo/src/components/Cartao.js
+++ React/ola-mundo/src/components/Cartao.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import faker from 'faker';
 
-const Cartao = () => {
+const Cartao = (props) => {
     return (
         <div className="ui card">
             <div className="image">
-                    <img src={faker.image.fashion()} />
+                    <img src={props.foto} />
             </div>
             <div className="content">
-                <a className="header">{faker.name.findName()}</a>
+                <a className="header">{props.nome}</a>
                 <div className="meta">
-                <span className="date">Joined in {faker.date.past().getFullYear()}</span>
+                <span className="date">Joined in {props.membroDesde}</span>
                 </div>
                 <div className="description">
-                {faker.lorem.sentence()}
+                {props.frase}
                 </div>
             </div>
             <div className="extra content">
                 <a>
                 <i className="user icon"></i>
-                {faker.random.number(3000)} Friends
+                {props.numeroAmigos} Friends
                 </a>
             </div>
         </div>
---
+++ React/ola-mundo/src/components/Mensagem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Mensagem = (props) => {
+    return <div>{`Olá ${props.nome}! Seja bem-vinda(o)!`}</div>
+}
+
+export default Mensagem;
