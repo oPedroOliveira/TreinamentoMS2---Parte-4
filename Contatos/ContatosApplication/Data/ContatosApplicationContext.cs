c8e3fb704e93d37c814e376ce70b3d5748eace23
oPedroOliveira <phoavl07@gmail.com>
2021-03-20 15:32:58 -0300

    Criando a API

---
+++ Contatos/ContatosApplication/Data/ContatosApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContatosApplication.Models;
+
+namespace ContatosApplication.Data
+{
+    public class ContatosApplicationContext : DbContext
+    {
+        public ContatosApplicationContext (DbContextOptions<ContatosApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contato>()
+                        .HasMany(n => n.Numeros)
+                        .WithOne()
+                        .HasForeignKey(p => p.ContatoId)
+                        .OnDelete(DeleteBehavior.Cascade);
+        }
+
+        public DbSet<ContatosApplication.Models.Contato> Contato { get; set; }
+    }
+}
